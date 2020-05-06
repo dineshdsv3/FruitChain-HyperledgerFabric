86cdfc1f46133cc40c1d45f480a3b77db48e9887
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-06 16:48:51 +0530

    Change in script to create and query new car

--- createCar.sh
+++ createCar.sh
@@ -7,5 +7,5 @@ CHAINCODE_SRC="github.com/chaincode"
 CHANNEL_NAME="mychannel"
 ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
-docker exec cli peer chaincode invoke -o orderer.example.com:7050  --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["createCar", "CAR1", "Honda", "Accord", "Black", "Tom"]}'
+docker exec cli peer chaincode invoke -o orderer.example.com:7050  --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["createCar", "CAR2", "Honda", "Accord", "White", "Dinesh"]}'
     
--- queryCar.sh
+++ queryCar.sh
@@ -6,5 +6,5 @@ CHAINCODE_NAME="mycc1"
 CHAINCODE_SRC="github.com/chaincode"
 CHANNEL_NAME="mychannel"
 
-docker exec cli peer chaincode query -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["queryCar", "CAR1"]}'
+docker exec cli peer chaincode query -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["queryCar", "CAR2"]}'
     
